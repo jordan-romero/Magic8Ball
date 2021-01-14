bf1366f644390c21ce249eca9ae8943f3424c841
jordles113 <jordan.t.romero@gmail.com>
2021-01-14 13:06:25 -0600

    Finishes basic layout for magic 8 ball. Not responsive yet. Dark mode, light mode toggle in progress.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var magicBall: UIImageView!
+    
+    @IBOutlet var toggleView: UIView!
+    
+    let ballArray = [#imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball5")]
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        magicBall.image = ballArray[Int.random(in: 0...4)]
     }
-
-
+    
+   
+    @IBAction func toggleLightMode(_ sender: UISwitch) {
+        toggleView.backgroundColor = .white
+    }
 }
 
